e1bd245d37e1c1b41c73c6b68046eb4634a4bceb
Vkasperski <victor.kasperski@laposte.net>
2017-05-04 17:53:43 +0200

    grrrr

---
+++ nParking/controller/data/membre.data.php
@@ -0,0 +1,20 @@
+<?php
+include("config/db.php");
+
+function getInfoConnect($mail){
+    $info = $GLOBALS["bdd"]->prepare("SELECT * FROM membre WHERE mail_membre = ?");
+    $info->execute($mail);
+    return $info;
+}
+
+function MailExiste($mail){
+	$verif=$GLOBALS["bdd"]->prepare("SELECT mail_membre AS  FROM membre WHERE mail_membre=?");
+  	$verif->execute(array($mail));
+  	return $verif;
+}
+
+function createMembreData($mail,$hashpsw,$civilite,$nom,$prenom,$date_naiss,$rue,$cp,$ville){
+	$r = $GLOBALS["bdd"]->prepare("INSERT INTO `membre` (`id_membre`, `mail_membre`, `psw_membre`, `civilite_membre`, `nom_membre`, `prenom_membre`, `date_naiss_membre`, `adRue_membre`, `adCP_membre`, `adVille_membre`, `rang`, `valide_membre`, `admin_membre`) VALUES (NULL, ?, ?, ?, ?, ?, ?, ?, ?, ?, '0', '0', '0')");
+    $r->execute(array($mail,$hashpsw,$civilite,$nom,$prenom,$date_naiss,$rue,$cp,$ville));
+}
+?>
---
+++ nParking/controller/membre.ctrl.php
@@ -0,0 +1,58 @@
+<?php
+
+include ("data/membre.data.php");
+
+
+
+//VERIFIE MAIL ET MOT DE PASSE
+function isValid($mail, $psw){
+    $i = getInfoConnect($mail);
+    $data = $i->fetch();
+    if ($data["psw_membre"] == $psw){
+    	
+        return true;
+    }
+    else{
+    	return false;
+    }
+}
+
+
+//VERIFIE SI LE MEMBRE A ÉTÉ VALIDÉ PAR UN ADMIN 
+function isValidMembre($mail){
+	$i = getInfoConnect($mail);
+	$data = $i->fetch();
+	if($data["valide_membre"] == 1){
+		return true;
+	}else{
+		return false;
+	}
+	
+}
+
+// HASH Du MOT DE PASS
+function hashMdp($psw) {
+
+
+// FONCTION MINIMALISTE, prenez contact pour une fonction plus sécurisé
+
+	return md5($psw);
+}
+
+//VERIFIE SI L'EMAIL EXISTE DÉJA
+function isMailExiste($mail){
+	$verif = mailExiste($mail);
+	$verif->fetch();
+	if($verif->rowCount() == 0){
+		return true;
+	}
+	return false;
+}
+
+
+//CREATION D'UN MEMBRE
+function createMembre($mail,$psw,$civilite,$nom,$prenom,$date_naiss,$rue,$cp,$ville){
+	$psw = hashMdp($psw);
+	createMembreData($mail,$psw,$civilite,$nom,$prenom,$date_naiss,$rue,$cp,$ville);
+}
+?>
