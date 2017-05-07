be23940f446f274fd0a07386bf0ed6c58f6aebb1
Asandolo <asandolo3@gmail.com>
2017-05-07 21:52:43 +0200

    lolole

--- nParking/reservation.php
+++ nParking/reservation.php
@@ -4,18 +4,18 @@ include('includes/pages/header.php');
 include('controller/reserver.ctrl.php');
 include('controller/place.ctrl.php');
 
-$m = getMembreById($_SESSION["id"]);
+//$m = getMembreById($_SESSION["id"]);
 
-if (!(getPlaceMembre($m["id"])==null && $m["rang"]<=0))
+if (!(getPlaceMembre($user["id"])==null && $user["rang"]<=0))
     header('Location: index.php');
 
-echo $m["id"];
+//echo $m["id"];
 
 reserver($_SESSION["id"]);
 
 ?>
 <div class="row">
-    <?php echo "DEBUG : ".$m["rang"]; ?>
+    <?php echo "DEBUG : ".$user["rang"]; ?>
     <div class="col-md-12 black">
         <p>Merci de votre réservation !</p>
         <a href="index.php">Retour</a>
